7453b8712a7ebb1c69f4db204d1ee5ad3759fdde
Lukas <Lukas.petersson.1999@gmail.com>
2020-12-14 23:18:20 +0100

    split dataset

--- data/createDataSet.py
+++ data/createDataSet.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
 
+# Create arrays with filenames and lables
 inFile = open("train-grouped.txt", "r")
 filenames = []
 lables = []
@@ -10,16 +11,30 @@ for i, v in enumerate(inFile):
 filenames = tf.constant(filenames)
 labels = tf.constant(lables)
 
+#create Dataset
 dataset = tf.data.Dataset.from_tensor_slices((filenames, labels))
 
-def _parse_function(filename, label):
-    image_string = tf.read_file(filename)
-    image_decoded = tf.image.decode_jpeg(image_string, channels=3)
-    image = tf.cast(image_decoded, tf.float32)
-    return image, label
+# load images
+def im_file_to_tensor(file, label):
+    def _im_file_to_tensor(file, label):
+        path = f"./frames/{file.numpy().decode()}"
+        im = tf.image.decode_jpeg(tf.io.read_file(path), channels=3)
+        im = tf.cast(image_decoded, tf.float32) / 255.0
+        return im, label
+    return tf.py_function(_im_file_to_tensor,
+                          inp=(file, label),
+                          Tout=(tf.float32, tf.uint8))
 
-dataset = dataset.map(_parse_function)
-dataset = dataset.batch(3)
+dataset = dataset.map(im_file_to_tensor)
 
-iterator = dataset.make_one_shot_iterator()
-images, labels = iterator.get_next()
+# include?
+#dataset.shuffle()
+
+#split dataset
+train_ds_size = int(0.7 * len(lables))
+valid_ds_size = int(0.15 * len(lables))
+
+train_dataset = dataset.take(train_ds_size)
+remaining = dataset.skip(train_ds_size)
+valid_dataset = remaining.take(valid_ds_size)
+test_dataset = remaining.skip(valid_ds_size)
